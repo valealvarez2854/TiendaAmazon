5dac21b283f141352d3c13aa03a36f7fe80145ca
valealvarez2854 <61520291+valealvarez2854@users.noreply.github.com>
2021-08-11 14:34:08 -0500

    boton limpiar

--- controlador.js
+++ controlador.js
@@ -1,84 +1,93 @@
-//Controlar actividad del formulario
+//CONTROLAR LA ACTIVIDAD DEL FORMULARIO
 let nombreConsola;
 let precioConsola;
 let pesoConsola;
 let fotoConsola;
-//1.controlar el boton
+
+//1. Controlar con JS el boton del formulario
 let boton=document.getElementById("boton");
 let botonLimpiar=document.getElementById("botonLimpiar");
 
-//2.detectar click en el boton
+//2. Detectar el clic en el boton
 boton.addEventListener("click",capturarDatos);
 botonLimpiar.addEventListener("click",limpiarCarrito);
-//3.crear funcion propia
+
+//3. Crear funcion PROPIA que se llamo en el punto 2
 function capturarDatos(){
 
-    //4.capturar los valores del formulario
-     let consolaElegida=document.getElementById("productos").value;
-     seleccionarConsola(consolaElegida);
+    //4. Capturar los valores del formulario
+    // creo tantas variables como valores del formulario
 
-     let cantidad=document.getElementById("cantidad").value;
+    let consolaElegida=document.getElementById("productos").value;
+    seleccionarConsola(consolaElegida);
+
+    let cantidad=document.getElementById("cantidad").value;
     
     let pildora=document.getElementById("pildora");
     pildora.textContent=cantidad;
     pildora.classList.remove("invisible");
     pildora.classList.add("visible");
+
     
     let direccion=document.getElementById("comentarios").value;
     
     let fotoCarrito=document.getElementById("fotoCarrito");
     fotoCarrito.src=fotoConsola;
-    console.log(fotoCarrito);
 
     let tituloCarrito=document.getElementById("tituloCarrito");
     tituloCarrito.textContent=nombreConsola;
 
-    
     let precioIndividual=document.getElementById("precioUnitarioCarrito");
-    precioIndividual.textContent=`Costo Unitario: $${precioConsola} USD`;
+    precioIndividual.textContent=`Costo unitario: $${precioConsola} USD`;
 
     let pesoIndividual=document.getElementById("pesoUnitarioCarrito");
-    pesoIndividual.textContent=`Peso Unitario: ${pesoConsola} LB`;
+    pesoIndividual.textContent=`Peso unitario: ${pesoConsola} Lb`;
+
+    let pesoTotal=document.getElementById("pesoTotalCarrito");
+    pesoTotal.textContent=`Peso Total del envio: ${pesoConsola*cantidad}Lb`;
     
-    let PesoTotalCarrito=document.getElementById("PesoTotalCarrito");
-    PesoTotalCarrito.textContent=`Peso Total de la Consola: ${pesoConsola*cantidad} LB`;
-     
-    let CostoCasillero=document.getElementById("CostoCasiollero");
-    CostoCasillero.textContent=`Costo Casillero: ${calcularCostoCasillero(pesoConsola,cantidad)} USD`;
+    let costoCasillero=document.getElementById("costoCasillero");
+    costoCasillero.textContent=`Costo Casillero: $${calcularCostoCasillero(pesoConsola,cantidad)} USD`;
     
-    let constoImpuesto=document.getElementById("costoImpuestos");
-    constoImpuesto.textContent=`Costo Venta (impuesto):$${calcularCostoImpuestos(precioConsola,cantidad)}`;
+    let costoImpuestos=document.getElementById("costoImpuestos");
+    costoImpuestos.textContent=`Costo venta(impuestos):$${calcularCostoImpuestos(precioConsola,cantidad)}`;
 
     let costoTotal=document.getElementById("costoTotal");
-    costoTotal.textContent=`Costo Total $${(calcularCostoCasillero (pesoConsola,cantidad))+(calcularCostoImpuestos)}`;
-    
+    costoTotal.textContent=`Costo Total: $${(calcularCostoCasillero(pesoConsola,cantidad))+(calcularCostoImpuestos(precioConsola,cantidad))} USD`;
+
 
 }
 
-// 5. selleccionar la informacion de la consola segun la seleccion del usuario
+//5. Seleccionar la información de la consola segun la selección del usuario
 function seleccionarConsola(opcion){
+
     let consolas={
         nombres:Array("PS5","XBOX Serie X", "Nintendo Switch"),
         precios:Array(750,700,409),
         pesos:Array(14.2,13.1,4.75),
-        fotos:Array("https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/ps5.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/xbox.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/nintendo.jpg?raw=true")
+        fotos:Array("https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/ps5.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/xbox.jpg?raw=true","https://github.com/jjosegallegocesde/imagenes-amazon/blob/main/img/nintendo.jpg?raw=true"),
+        
     }
+
     if(opcion==1){
+
         nombreConsola=consolas.nombres[0];
         precioConsola=consolas.precios[0];
         pesoConsola=consolas.pesos[0];
         fotoConsola=consolas.fotos[0];
 
     }else if(opcion==2){
+
         nombreConsola=consolas.nombres[1];
         precioConsola=consolas.precios[1];
         pesoConsola=consolas.pesos[1];
         fotoConsola=consolas.fotos[1];
 
     }else if(opcion==3){
+
         nombreConsola=consolas.nombres[2];
         precioConsola=consolas.precios[2];
-        pesoConsola=consolas.pesos[2]
+        pesoConsola=consolas.pesos[2];
         fotoConsola=consolas.fotos[2];
 
     }else{
@@ -86,37 +95,46 @@ function seleccionarConsola(opcion){
         precioConsola=null;
         pesoConsola=null;
         fotoConsola=null;
+
     }
+
+
 }
-//2. Calcular el costo del casillero
+
 function calcularCostoCasillero(pesoConsola,cantidad){
 
-    let CostoCasillero=0;
+    let costoCasillero=0;
     let pesoTotalEnvio=pesoConsola*cantidad;
 
     if(pesoTotalEnvio<=20){
 
-        CostoCasillero=85;
+        costoCasillero=85;
 
-    }else{ //Me paso del paso
+    }else{ 
 
         let pesoExtra=pesoTotalEnvio-20;
-        CostoCasillero=85+(pesoExtra*2);
+        costoCasillero=85+(pesoExtra*2);
 
     }
     
-    return CostoCasillero;
+    return costoCasillero;
 }
+
 function calcularCostoImpuestos(costoConsola,cantidad){
+
     const IMPUESTO_PAIS=114;
     const VALOR_SEGURO=7;
+
     let costoCompra=costoConsola*cantidad;
     let costoTotal=costoCompra+IMPUESTO_PAIS+VALOR_SEGURO;
+
     return costoTotal;
+
 } 
 
-function limpiarCarrito(){
-    console.log("Hiciste clic en limpiar");
 
+function limpiarCarrito(){
+
+    console.log("Hiciste clic en limpiar");
 
 }
